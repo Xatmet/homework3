5d504a98e490b08eb5ffe0d71d56e69374498131
Xatmet <xatuna.meteeva@mail.ru>
2023-01-16 02:00:12 +0300

    выполнили 6 задание

--- Program.cs
+++ Program.cs
@@ -1,111 +1,49 @@
-﻿//Задайте массив заполненный случайными положительными трёхзначными числами.
-// Напишите программу, которая покажет количество чётных чисел в массиве.
-//[345, 897, 568, 234] -> 2
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-int[] array = new int[10];
-System.Console.WriteLine("Введите  трехзначное число:");
-int sum = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-SArray(array);
-PrintArray(array);
-for (int i = 0; i < array.Length; i++)
+// 0, 7, 8, -2, -2 -> 2
 
-    if (array[i] % 2 == 0)
-        count++;
-System.Console.WriteLine($"всего {array.Length} чисел,{count} из них четные");
-void SArray(int[] array)
-{
-}
-for (int i = 0; i < array.Length; i++)
-{
-    array[i] = new Random().Next(100, 1000);
-}
-
-//void PrintArray(int[] array)
-//{
-//  System.Console.WriteLine();
-//for (int i = 0; i < array.Length; i++)
-//{
-System.Console.WriteLine();
-}
-}
-
-
-
-// Задача 36: Задайте одномерный массив, заполненный случайными числами.
-// Найдите сумму элементов, стоящих на нечётных позициях.
-//[3, 7, 23, 12] -> 19
-//[-4, -6, 89, 6] -> 0
-
+// 1, -7, 567, 89, 223-> 3
 
 System.Console.WriteLine("Введите  число массива:");
-int sum = Convert.ToInt32(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[10];
-int summa = 0;
-Summa(array);
+int sum = 0;
+Sum(array);
 PrintArray();
-int Summa(int[] array)
+int Sum(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(0, 100);
-        System.Console.WriteLine($"{array[i]} ");
-        if (array[i] % 2! == 1)
+        array[i] = new Random().Next(-10, 10);
+        System.Console.Write($"{array[i]} ");
+        if (array[i] > 0)
         {
-            summa = summa + array[i];
+            sum = array[i] + 1;
         }
     }
-    return summa;
+    return sum;
 }
 void PrintArray()
 {
-    System.Console.WriteLine($"сумма элементов на нечетных позициях равна:{summa} ");
-}
-
-
-//Задача 38: Задайте массив вещественных чисел. 
-//Найдите разницу между максимальным и минимальным элементов массива.
-//[3 7 22 2 78] -> 76
-
-System.Console.WriteLine("Введите  число массива:");
-int sum = Convert.ToInt32(Console.ReadLine());
-double[] array = new double[5];
-SArray(array);
-PrintArray(array);
-double max = Int32.MaxValue;
-double min = Int32.MinValue;
-for (int i = 0; i < array.Length; i++)
-{
-    array[i] = new Random().Next(0, 100);
-
-    System.Console.WriteLine($"{array[i]} ");
-    if (array[i] > max)
-    {
-        max = array[i];
-    }
-    if (array[i] < min)
-    {
-        min = array[i];
-    }
-}
-System.Console.WriteLine($"максимальное значение {max}, минимальное значение {min}");
-System.Console.WriteLine($"разниц между максимальным и минимальным значение {max - min}");
-void SArray(double[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = Convert.ToDouble(new Random().Next(10, 100)) / 10;
-    }
-}
-void PrintArray(double[] array)
-{
-    System.Console.WriteLine();
-    for (int i = 0; i < array.Length; i++)
-    {
-        System.Console.Write($"{array[i]} ");
-    }
+    System.Console.WriteLine($"количество чисел больше 0 равно = : {sum} ");
 }
 
 
 
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
 
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите число b1 : ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k1 : ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b2 : ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k2 : ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"точка пересечения двух прямых X: {x}, Y: {y} ");
