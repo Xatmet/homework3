0ac7918447e6ace72ce3874d6d65feae2e75af8d
Xatmet <xatuna.meteeva@mail.ru>
2023-01-18 22:00:52 +0300

    выполнили 7 задание

--- Program.cs
+++ Program.cs
@@ -1,49 +1,102 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
 
-// 0, 7, 8, -2, -2 -> 2
+// m = 3, n = 4.
 
-// 1, -7, 567, 89, 223-> 3
+// 0,5 7 -2 -0,2
 
-System.Console.WriteLine("Введите  число массива:");
-int num = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[10];
-int sum = 0;
-Sum(array);
-PrintArray();
-int Sum(int[] array)
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+
+Console.WriteLine("Введите число m : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n : ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m, n];
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i] = new Random().Next(-10, 10);
-        System.Console.Write($"{array[i]} ");
-        if (array[i] > 0)
-        {
-            sum = array[i] + 1;
-        }
+        array[i,j] = Convert.ToDouble(new Random().Next(1, 100)) / 10;
+        Console.Write($"{array[i, j]} ");
     }
-    return sum;
+    Console.WriteLine();
 }
-void PrintArray()
+
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    System.Console.WriteLine($"количество чисел больше 0 равно = : {sum} ");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Введите число: ");
+int f = Convert.ToInt32(Console.ReadLine());
+if (m >= array.GetLength(0) || n >= array.GetLength(1))
+    Console.WriteLine("такого числа в массиве нет");
+else
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
 }
 
 
+// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
 
-// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
-//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
-// значения b1, k1, b2 и k2 задаются пользователем.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-
-Console.WriteLine("Введите число b1 : ");
-double b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число k1 : ");
-double k1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число b2 : ");
-double b2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число k2 : ");
-double k2 = Convert.ToInt32(Console.ReadLine());
-double x = -(b1 - b2) / (k1 - k2);
-double y = k1 * x + b1;
-Console.WriteLine($"точка пересечения двух прямых X: {x}, Y: {y} ");
+Console.WriteLine("Введите количество строк n : ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов m : ");
+int m = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m, n];
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double average = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        System.Console.Write($"{array[i, j]} ");
+        average = (average + array[i, j]);
+    }
+    average = average / m;
+    Console.Write($"среднее арифметическое столбца равно : {average} ");
+    Console.WriteLine();
+}
