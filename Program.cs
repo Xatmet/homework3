bae76336d4621fc4d14e99061f67aab4725b82a8
Xatmet <xatuna.meteeva@mail.ru>
2023-01-20 05:15:16 +0300

    Выполнила итоговую работу, Ура!!!Станислав, Благодарю ;)

--- Program.cs
+++ Program.cs
@@ -1,134 +1,66 @@
-﻿
-// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// В итоге получается вот такой массив:
-// 7 4 2 1
-// 9 5 3 2
-// 8 4 4 2
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке
+//  от N до 1. Выполнить с помощью рекурсии.
 
-Console.WriteLine("Введите число: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[,] array = new int[m, n];
-Console.WriteLine("");
-Console.WriteLine("изначальный массив: ");
-for (int i = 0; i < array.GetLength(0); i++)
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+int Nat(int num)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
+    if (num == 1)
     {
-        array[i, j] = new Random().Next(0, 10);
-        Console.Write($"{array[i, j]} ");
+        return 1;
     }
-    Console.WriteLine("");
+    Console.Write(num);
+    return Nat(num - 1);
 }
-for (int i = 0; i < array.GetLength(0); i++)
+int x = Nat(num);
+Console.WriteLine(x);
+
+
+// // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке
+// //  от M до N.
+
+// // M = 1; N = 15 -> 120
+// // M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M : ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Sum(int M, int N)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
+    if (N == M)
     {
-        for (int z = 0; z < array.GetLength(1) - 1; z++)
-        {
-            if (array[i, z] < array[i, z + 1])
-            {
-                int temp = 0;
-                temp = array[i, z];
-                array[i, z] = array[i, z + 1];
-                array[i, z + 1] = temp;
-            }
-        }
+        return M;
     }
+    return N + Sum(M, N - 1);
 }
-Console.WriteLine("");
-Console.WriteLine("упорядоченный массив: ");
-PrintArray(array);
-void PrintArray(int[,] arr)
+int x = Sum(M, N);
+Console.WriteLine(x);
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа
+//  m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите число M : ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Ackerzman(int N, int M)
 {
-
-    for (int i = 0; i < array.GetLength(0); i++)
+    if (N == 0)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} ");
-        }
-        Console.WriteLine("");
+        return M + 1;
     }
+    else if (M == 0)
+    {
+        return Ackerzman(N - 1, 1);
+    }
+    else return Ackerzman(N - 1, Ackerzman(N, M - 1));
 }
-
-// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
-// которая будет находить строку с наименьшей суммой элементов.
-// Например, задан массив:
-
-// 1 4 7 2
-
-// 5 9 2 3
-
-// 8 4 2 4
-
-// 5 2 6 7
-
-// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов:1 строка
- 
-Console.WriteLine("Введите число: ");                        //это решение находит сумму всех строк
-int m = Convert.ToInt32(Console.ReadLine());             
-Console.WriteLine("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[,] array = new int[m, n];
-for (int j = 0; j < array.GetLength(1); j++)
-{
-    int sum = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        array[i, j] = new Random().Next(0, 10);
-        System.Console.Write($"{array[i, j]} ");
-        sum = (sum + array[i, j]);
-    }
-    Console.Write($" {sum} ");
-
-    Console.WriteLine();
-}
-
-
-
-Console.WriteLine("Введите число: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[,] array = new int[m, n];
-int count = 0;
-int minsum = Int32.MaxValue;
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    int sum = 0;
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-
-        array[i, j] = new Random().Next(0, 10);
-        sum = (sum + array[i, j]);
-    }
-    if (sum < minsum)
-    {
-        minsum = sum;
-        count++;
-        Console.WriteLine("строка с наименьшей суммой элементов - " + (count) + " , " + (minsum));
-    }
-}
-PrintArray(array);
-void PrintArray(int[,] arr)
-{
-
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} ");
-        }
-
-        Console.WriteLine("");
-    }
-}
-
-
-
+int x = Ackerzman(M, N);
+Console.WriteLine(x);
